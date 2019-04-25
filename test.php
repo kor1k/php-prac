516e8d54f0e96189a54334ac4ced85db2382bb0d
kor1k <koortttt@gmail.com>
2019-04-25 11:19:01 +0300

    added prac for diff file

--- test.php
+++ test.php
@@ -1,4 +1,59 @@
 <?php
-  echo 'Hello world';
-  echo 'Hello world #2';
+        $a = 100;
+        // $b = 14.88;
+        // $c = 'YASCHIK';
+        // $d = array(12,13,'adasdad');
+        // $e= array(
+        //     'name' => 'Kostya',
+        //     'lastName' => 'Yelskiy',
+        //     'age' => 19,
+        //     'birth' => 1999,
+        //     'edu' => array(
+        //         'DUT',
+        //         'DAN-IT.Edu'
+        //     )
+            // mnogomerniy array
+        // );
+        // echo $a;
+        // echo $b;
+        // echo $c;
+        // echo $d[0];
+        // echo $d[2];  
+        // echo $e['birth'];
+        // echo $e['age'];
+        // echo $e['edu'][1];
+    //     echo 'First variable: $a<br>';
+    //     echo "test: $a";
+    // $name = "Kostya";
+    // $surname = "Yelskiy";
+    // echo $name;
+    // echo ' ';
+    // echo $surname;
+    // echo $name . $surname;
+    // $asd = 12;
+    // $dsa = 543;
+    // echo 'Ответ: ' . ($asd * $dsa);
+    // $weather = 'clouds'; //snow, rain,//sunny
+    // if ($weather == 'snow') {
+    //     echo "ok - snowing";
+    // }
+    // if ($weather == 'rain') {
+    //     echo "ok - raining";
+    // }
+    // if ($weather == 'sunny') {
+    //     echo "ok - sunny";
+    // }
+    // if ($weather == 'clouds') {
+    //     echo "ok - clouds";
+    // }
+
+    $age = 17;
+    if($age == 18 || $age >= 21) {
+        echo 'You allowed';
+    } else {
+        echo 'Not allowed';
+    }
+    // if($age >= 21) {
+    //     echo 'welcome!';
+    // }
 ?>
